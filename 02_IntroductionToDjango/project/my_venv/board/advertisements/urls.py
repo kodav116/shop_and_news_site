8851072d74e7f9b5396215b705559b33ba42f271
Konst24 <darthtogors@gmail.com>
2022-09-15 00:18:26 +0300

    Выполнена практическая работа.

--- 02_IntroductionToDjango/project/my_venv/board/advertisements/urls.py
+++ 02_IntroductionToDjango/project/my_venv/board/advertisements/urls.py
@@ -3,5 +3,10 @@ from .import views
 
 
 urlpatterns = [
-    path('', views.advertisement_list, name='advertisement_list')
+    path('', views.advertisement_list, name='advertisement_list'),
+    path('first_course', views.first_course, name='first_course'),
+    path('second_course', views.second_course, name='second_course'),
+    path('third_course', views.third_course, name='third_course'),
+    path('fourth_course', views.fourth_course, name='fourth_course'),
+    path('fifth_course', views.fifth_course, name='fifth_course')
 ]
--- 02_IntroductionToDjango/project/my_venv/board/advertisements/views.py
+++ 02_IntroductionToDjango/project/my_venv/board/advertisements/views.py
@@ -4,3 +4,24 @@ from django.http import HttpResponse
 
 def advertisement_list(request, *args, **kwargs):
     return render(request, 'advertisements/advertisement_list.html', {})
+
+
+def first_course(request, *args, **kwargs):
+    return render(request, 'advertisements/first_course.html', {})
+
+
+def second_course(request, *args, **kwargs):
+    return render(request, 'advertisements/second_course.html', {})
+
+
+def third_course(request, *args, **kwargs):
+    return render(request, 'advertisements/third_course.html', {})
+
+
+def fourth_course(request, *args, **kwargs):
+    return render(request, 'advertisements/fourth_course.html', {})
+
+
+def fifth_course(request, *args, **kwargs):
+    return render(request, 'advertisements/fifth_course.html', {})
+
