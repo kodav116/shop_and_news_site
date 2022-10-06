7a1eb707c88bcebcf3e97ccc775b8e7a0ae4103b
Konst24 <darthtogors@gmail.com>
2022-10-06 23:08:42 +0300

    Сделано 4 практическая работа

--- 02_IntroductionToDjango/project/my_venv/board/advertisements/urls.py
+++ 02_IntroductionToDjango/project/my_venv/board/advertisements/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .import views
+from . import views
 
 
 urlpatterns = [
     path('', views.advertisement_list, name='advertisement_list'),
-    path('first_course', views.first_course, name='first_course'),
-    path('second_course', views.second_course, name='second_course'),
-    path('third_course', views.third_course, name='third_course'),
-    path('fourth_course', views.fourth_course, name='fourth_course'),
-    path('fifth_course', views.fifth_course, name='fifth_course')
-]
+    path('main/advertisements/', views.Advertisements.as_view()),
+    path('main/about/', views.About.as_view()),
+    path('main/contacts/', views.Contacts.as_view()),
+    path('main/', views.Main.as_view())
+    ]
--- 02_IntroductionToDjango/project/my_venv/board/advertisements/views.py
+++ 02_IntroductionToDjango/project/my_venv/board/advertisements/views.py
@@ -1,27 +1,76 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 
 
 def advertisement_list(request, *args, **kwargs):
-    return render(request, 'advertisements/advertisement_list.html', {})
+    advertisements = [
+        'Мастер на час',
+        'Мастер на день',
+        'Мастер на неделю',
+        'Мастер на месяц'
+    ]
+
+    return render(request, 'advertisements/advertisement_list.html', {'advertisements': advertisements})
 
 
-def first_course(request, *args, **kwargs):
-    return render(request, 'advertisements/first_course.html', {})
+class Advertisements(View):
+    def get(self, request):
+        advertisements = [
+            'Мастер на час',
+            'Мастер на день',
+            'Мастер на неделю',
+            'Мастер на месяц'
+        ]
+        return render(request, 'advertisements/advertisements.html', {'advertisements': advertisements})
+
+    def post(self, request):
+        print(request.POST, 'Запрос на запись успешно выполнен.')
+        return render(request, 'advertisements/advertisements.html')
 
 
-def second_course(request, *args, **kwargs):
-    return render(request, 'advertisements/second_course.html', {})
+class About(TemplateView):
+    template_name = 'advertisements/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Объявления'
+        context['description'] = """
+        Хотите быстро купить что-то или продать?
+
+        Вывесите объявление здесь!
+        """
+        return context
 
 
-def third_course(request, *args, **kwargs):
-    return render(request, 'advertisements/third_course.html', {})
+class Contacts(TemplateView):
+    template_name = 'advertisements/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['address'] = 'г. Краснодар, ул. Очаковская 45'
+        context['phone'] = '89999999999'
+        context['mail'] = 'django@mail.com'
+        return context
 
 
-def fourth_course(request, *args, **kwargs):
-    return render(request, 'advertisements/fourth_course.html', {})
+class Main(TemplateView):
+    template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['about'] = 'О компании'
+        context['contacts'] = 'Наши контакты'
+        context['advertisements'] = 'Наши объявления'
+        context['aboutURL'] = 'advertisements/about/'
+        context['contactsURL'] = 'advertisements/contacts/'
+        context['advertisementsURL'] = 'advertisements/advertisements/'
+        return context
+
+    def post(self, request):
+        print(request.POST, 'Запрос на запись успешно выполнен.')
+        return render(request, 'main.html')
 
 
-def fifth_course(request, *args, **kwargs):
-    return render(request, 'advertisements/fifth_course.html', {})
 
