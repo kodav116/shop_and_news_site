93961d3b6b16c87e4c8de0da0a5c1ecd737c9c05
Konst24 <darthtogors@gmail.com>
2023-01-31 16:48:37 +0300

    Внесение правок

--- 02_IntroductionToDjango/project/my_venv/board/advertisements/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.advertisement_list, name='advertisement_list'),
-    path('adv_one', views.adv_one, name='adv_one'),
-    path('adv_two', views.adv_two, name='adv_two')
-]
--- 09_Files/djfiles/app_media/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppMediaConfig(AppConfig):
-    name = 'app_media'
